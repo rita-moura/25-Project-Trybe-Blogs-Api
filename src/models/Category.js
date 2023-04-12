813cfcf0b3caea2c0a0a6151c01da7075f042e60
Rita Moura <ritamoura520@gmail.com>
2023-04-12 11:52:21 -0300

    Fix: corrige a model Category

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,5 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
   const CategoryTable = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
     name: DataTypes.STRING,
   }, {
     timestamps: false,
