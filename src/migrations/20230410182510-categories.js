40fa494aa003132b607f82c8780f0740f326224f
Rita Moura <ritamoura520@gmail.com>
2023-04-13 14:39:45 -0300

    Fix: corrige a referencia da tabela categories

--- src/migrations/20230410182510-categories.js
+++ src/migrations/20230410182510-categories.js
@@ -12,14 +12,7 @@ module.exports = {
       name: {
         type: Sequelize.STRING,
         allowNull: false,
-      },
-      post_id: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'blog_posts',
-          key: 'id',
-        }
-      },
+      }
     });
   },
 
